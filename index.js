9d131edeee99edcb2809d97f28d5d7a0abdb6333
farhana988 <farhana744255@gmail.com>
2024-12-23 11:17:43 +0600

    get a service by id for details page added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000
-require('dotenv').config()
-app.use(cors())
-app.use(express.json())
-// app.use(cookieParser())
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ljf3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,59 +20,59 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     // db create
-    const db = client.db('CriticizeIt')
-    const servicesCollection = db.collection('services')
-
-
+    const db = client.db("CriticizeIt");
+    const servicesCollection = db.collection("services");
 
     // post a service
-    app.post('/add-service', async (req, res) => {
-      const servicesData = req.body
-      const result = await servicesCollection.insertOne(servicesData)
-      res.send(result)
-    })
+    app.post("/add-service", async (req, res) => {
+      const servicesData = req.body;
+      const result = await servicesCollection.insertOne(servicesData);
+      res.send(result);
+    });
 
+    // get all services data
+    app.get("/all-services", async (req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
 
-      // get all services data from db
-      app.get('/all-services', async (req, res) => {
-        const filter = req.query.filter;
-        const search = req.query.search;
-    
-        let query = {};
-    
-        if (search) {
-            query.serviceTitle = {
-                $regex: search,
-                $options: "i",
-            };
-        }
-  
-        if (filter) {
-            query.category = filter;
-        }
-    
-        const result = await servicesCollection.find(query).toArray();
+      let query = {};
 
-        res.send(result);
+      if (search) {
+        query.serviceTitle = {
+          $regex: search,
+          $options: "i",
+        };
+      }
+
+      if (filter) {
+        query.category = filter;
+      }
+
+      const result = await servicesCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // get featured services data 
+    app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find().limit(6).toArray();
+      res.send(result);
     });
     
 
-
-      // get featured services data from db
-      app.get('/services', async (req, res) => {
-        const result = await servicesCollection.find().limit(6).toArray()
-        res.send(result)
-      })
-
-
-
+    // get a single service data by id 
+    app.get("/serviceDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
 
 
 
@@ -88,15 +88,13 @@ async function run() {
 
 
 
-
-
-
-  
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -104,11 +102,8 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello ");
+});
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello ')
-  })
-  
-  app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
