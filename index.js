b2fb6ff8c276e7cd507432e4dc780d00ced46673
farhana988 <farhana744255@gmail.com>
2024-12-23 18:49:10 +0600

    delete

--- index.js
+++ index.js
@@ -88,6 +88,13 @@ async function run() {
       const result = await servicesCollection.find(query).toArray()
       res.send(result)
     })
+    // delete a review
+    app.delete('/service/:id', async (req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await servicesCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
